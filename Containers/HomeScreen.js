05ae13383817698db43f0c3888719d3d123edc42
Mostafa Nawara <nawaragfx@gmail.com>
2017-08-10 17:33:54 +0200

    Add home page cards

--- Containers/HomeScreen.js
+++ Containers/HomeScreen.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import {
+  View,
+  Image,
   AppRegistry,
   Text,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Card, Button, Divider } from 'react-native-material-design';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home Screen',
   }
   render() {
-    return <Text>Hello, Home!</Text>;
+    let cardsList = [2, 3, 4, 5, 6];
+    return (
+        <View>
+            <Card>
+                <Card.Media
+                    image={<Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />}
+                    overlay
+                />
+                <Card.Body>
+                    <Text>Some text to go in the body.</Text>
+                </Card.Body>
+                <Card.Actions position="right">
+                    <Button value="ACTION" />
+                </Card.Actions>
+            </Card>
+            <Divider />
+            <Card>
+                <Card.Media
+                    image={<Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />}
+                    overlay
+                />
+                <Card.Body>
+                    <Text>Some text to go in the body.</Text>
+                </Card.Body>
+                <Card.Actions position="right">
+                    <Button value="ACTION" />
+                </Card.Actions>
+            </Card>
+            <Divider />
+            <Card>
+                <Card.Media
+                    image={<Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} />}
+                    overlay
+                />
+                <Card.Body>
+                    <Text>Some text to go in the body.</Text>
+                </Card.Body>
+                <Card.Actions position="right">
+                    <Button value="ACTION" />
+                </Card.Actions>
+            </Card>
+            <Divider />
+        </View>
+    );
   }
 }
 
